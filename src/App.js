d2393093f40daabafd7ba429e28f7ae8c0538663
Dylan Fox <dylan.fox.kc@gmail.com>
2022-08-03 17:33:42 -0500

    update text

--- src/App.js
+++ src/App.js
@@ -58,16 +58,45 @@ function App() {
         <Carousel.Item>
           <h5>Graduate Student <span>//</span> Graceland University</h5>
           <p>
-            I started my MSc in Data Science & Analytics degree at Graceland University in January 2022. Since then, I've completed courses in data science basics,
-            how to extract and clean data sets, machine learning, regression analysis, and working with big data.
+            I started my MSc in Data Science & Analytics degree at Graceland University in January 2022. Working in analytics developments and studying data science 
+            at the same time allow me to widen my view of the entire data lifecycle. I have a better understanding of how analytics strategies are developed, implemented,
+            sent downstream, and used for analysis to produce actionable insights.
           </p>
           <p>
-            Working through my MSc degree alongside working in analytics development has proven to be time-consuming but very beneficial. I have been able to apply
-            knowledge I've learned while working directly in my courses and vice versa.
+            Here are some topics I've studied.
           </p>
+          <ul>
+            <li>Data science basics</li>
+            <li>Extracting and cleaning data sets</li>
+            <li>Machine learning techniques</li>
+            <li>Regression analysis</li>
+            <li>Working with big data</li>
+            <li>R, Python</li>
+          </ul>
         </Carousel.Item>
         <Carousel.Item>
           <h5>Undergraduate Student <span>//</span> Graceland University</h5>
+          <p>
+            I spent my college years at Graceland University from 2017-2021. Throughout my time there, Graceland allowed me to explore and learn about 
+            my passions and garner many skills that came from unique opportunities that are only available at small schools.
+          </p>
+          <p>
+            I received Bachelors of Science degrees in Computer Science & Information Technology, Data Science, and Mathematics. I was involved with many extracurriculars: 
+            the Honors Program, the Ackerley Scholars Program, Academic Student Council, Student Government, Residence Life, Enactus, Association for Computing Machinery, 
+            and played varsity soccer.
+          </p>
+          <p>
+            Here are some of my favorites.
+          </p>
+          <ul>
+            <li>Serving as captain for the Men's Soccer team</li>
+            <li>Successfully campaigning to serve as the Academic Student Council President</li>
+            <li>Being elected by my peers to serve as a House President (RA+)</li>
+          </ul>
+          <p>
+            These opportunities significantly shaped and enhanced my college experience. They allowed me to develop my own leadership style, learn how to work 
+            with diverse individuals, and how to build a success-driven culture.
+          </p>
         </Carousel.Item>
       </Carousel>
     );
