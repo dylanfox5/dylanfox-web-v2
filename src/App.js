a623362c9493252934ac5603490a566bc29d7871
Dylan Fox <dylan.fox.kc@gmail.com>
2022-08-06 21:25:23 -0500

    animate on scroll

--- src/App.js
+++ src/App.js
@@ -8,10 +8,26 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from 'react-bootstrap/Button';
 
+import { useEffect } from 'react';
+import { useAnimation, motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  const variant = {
+    visible: { x: 0, transition: { ease: "easeInOut", duration: 0.75 } },
+    hidden: { x: -500 }
+  };
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
 
   var projects = [
     { title: "v3", description: "The third iteration of my portfolio website", link: "https://github.com/dylanfox5/dylanfox-v3", id: 1 },
@@ -28,36 +44,42 @@ function App() {
       <Container className="Home" id="home">
         <Row>
           <Col sm={12} lg={8} className="animate__animated animate__fadeInLeft animate__delay-1s">
-            <h3>Hi, I"m <span>Dylan</span> -- an analytics focused software developer who enjoys innovating solutions for complex problems.</h3>
+            <h3>Hi, I'm <span>Dylan</span> -- an analytics focused software developer who enjoys innovating solutions for complex problems.</h3>
           </Col>
         </Row>
       </Container>
       <Container id="about" className="About">
         <Row>
-          <Col sm={12} lg={8} >
-            <h1 className="about-title">about-me</h1>
-            <h5>
-              Leadership <span>//</span> Innovation <span>//</span> Problem-Solving
-            </h5>
-            <p>
-              I"m a strong advocate for serving others; choosing to have
-              the humility to put others first and care for others. A good leader must first be a good follower.
-              These are the foundations of <span>leadership</span>.
-            </p>
-            <p>
-              Continuously inventing and reinventing how I can serve others & society. The term
-              Futurist is one I often use to describe myself. The future and its possibilities excite me.
-              I think our potential as humans are limitless when <span>innovation</span> is at the forefront.
-            </p>
-            <p>
-              The art of understanding an obstacle, how it works, and how it can be overcome, is one
-              I find nothing short of beautiful. The leaders that bring forth our future and open its
-              possibilities are nothing without the vision of <span>problem-solving</span>.
-            </p>
-            <p>
-              This is the ideology I live each day by. Be sure to continue on or follow me on any social media
-              to see what that looks like. 🚀
-            </p>
+          <Col sm={12} lg={8}>
+            <motion.div
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={variant}>
+              <h1 className="about-title">about-me</h1>
+              <h5>
+                Leadership <span>//</span> Innovation <span>//</span> Problem-Solving
+              </h5>
+              <p>
+                I'm a strong advocate for serving others; choosing to have
+                the humility to put others first and care for others. A good leader must first be a good follower.
+                These are the foundations of <span>leadership</span>.
+              </p>
+              <p>
+                Continuously inventing and reinventing how I can serve others & society. The term
+                Futurist is one I often use to describe myself. The future and its possibilities excite me.
+                I think our potential as humans are limitless when <span>innovation</span> is at the forefront.
+              </p>
+              <p>
+                The art of understanding an obstacle, how it works, and how it can be overcome, is one
+                I find nothing short of beautiful. The leaders that bring forth our future and open its
+                possibilities are nothing without the vision of <span>problem-solving</span>.
+              </p>
+              <p>
+                This is the ideology I live each day by. Be sure to continue on or follow me on any social media
+                to see what that looks like. 🚀
+              </p>
+            </motion.div>
           </Col>
         </Row>
       </Container>
